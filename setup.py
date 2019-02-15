7d82ecff169e3d265734f2e1adf788132947fc21
Audrey <audrey.difernand@etu.univ-amu.fr>
2019-02-15 14:32:06 +0100

    Premier message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='identification des deserts medicaux',
+    author='mag3',
+    license='MIT',
+)
